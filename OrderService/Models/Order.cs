1e90c1e0795535084a995204abcb4a2367aa0770
Elizaveta Lavrenova <e.lavr95@gmail.com>
2017-10-10 14:50:19 +0300

    Project files were added

---
+++ OrderService/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderService.Models
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public long StockId { get; set; }
+        public int Weight { get; set; }
+        public long TransferId { get; set; }
+        public int Status { get; set; }
+    }
+}
